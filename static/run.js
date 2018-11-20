3cb7e62ab450e7f0e94c0569f3da35603497a763
TheDripper <tylerehill@gmail.com>
2018-11-20 14:00:45 -0800

    cross browser wheel working

--- static/run.js
+++ static/run.js
@@ -1,31 +1,52 @@
-const throttle = (func, limit) => {
-  let inThrottle
+function throttle(func, wait, options) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  if (!options) options = {};
+  var later = function() {
+    previous = options.leading === false ? 0 : Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
   return function() {
-    const args = arguments
-    const context = this
-    if (!inThrottle) {
-      func.apply(context, args)
-      inThrottle = true
-      setTimeout(() => inThrottle = false, limit)
+    var now = Date.now();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
     }
-  }
-}
-document.addEventListener('wheel',throttle((e)=>{
+    return result;
+  };
+};
+
+
+document.addEventListener('wheel',throttle(function(e){
 	var view = document.querySelector('#viewer');
 	console.log(view.scrollLeft);
 	if (e.deltaY > 0) {
 		console.log('up')
-		view.scrollBy({
-			left: -window.innerWidth,
-			behavior: 'smooth'
-		})
+		//view.scrollBy({
+		//	left: -window.innerWidth,
+		//	behavior: 'smooth'
+		//})
 		//view.scrollLeft -= window.innerWidth
 	} else if (e.deltaY < 0) {
 		console.log('down')
-		view.scrollBy({
-			left: window.innerWidth,
-			behavior: 'smooth'
-		})
+		//view.scrollBy({
+		//	left: window.innerWidth,
+		//	behavior: 'smooth'
+		//})
 		//view.scrollLeft += window.innerWidth
 	}
 },1000));
