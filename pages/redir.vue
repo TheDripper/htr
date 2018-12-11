f6c047de862c2b213226f90700aa0da07c879584
TheDripper <tylerehill@gmail.com>
2018-12-11 08:34:45 -0800

    url loader working

---
+++ pages/redir.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  asyncData ({ redirect, store }) {
+	  if(process.browser) {
+		let id = new URL(window.location).pathname.replace(/\//g, "");
+		let pages = {
+			"home": 0,
+			"mission": 1,
+			"coalition": 2
+		}
+		store.commit('setID',id)
+		store.commit('setCur',pages[id])
+    		return redirect('/')
+	  }
+  }
+}
+</script>
