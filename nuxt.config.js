19518b17634e6d2fc4678f67ab740fd5fbaa1145
TheDripper <tylerehill@gmail.com>
2018-12-11 12:42:58 -0800

    config fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,9 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'universal',
-  env: {
-	baseUrl: 'http://localhost:3000'
-  },
+  //env: {
+  //      baseUrl: 'http://localhost:3000'
+  //},
   //router: {
   //        base: '/dist/'
   //},
