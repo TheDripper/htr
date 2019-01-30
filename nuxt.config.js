329cbe950324588fdfcbb50aa0c8dd0c6018fa0f
TheDripper <tylerehill@gmail.com>
2019-01-29 16:28:10 -0800

    tree hover start

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,7 @@ module.exports = {
     script: [
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' },
       { src: '/mailer.js' },
+      { src: '/stage/tree.js' },
       { src: 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js' }
     ]
   },
